6298d03bc3a1fce4151a055909c894ae705ed624
levvnew <67289988+levvnew@users.noreply.github.com>
2022-03-09 09:13:39 +0300

    Add files via upload

---
+++ YaPr 11.10.1-4 Yandex Music.py
@@ -0,0 +1,8 @@
+import pandas as pd
+df = pd.read_csv('music_log_upd.csv')
+df.tail()
+df.tail().groupby('user_id')
+#print(df) #провека работает
+df.tail().groupby('user_id').sum() 
+current_engagement = df.tail().groupby('user_id').sum().median()
+print(current_engagement) #провека работает
