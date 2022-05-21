bd9adb7d025ae5cc778b2aa8c14929cbfba4cf29
levvnew <67289988+levvnew@users.noreply.github.com>
2022-05-21 23:25:54 +0300

    Delete YaPr 11.10.1-4 Yandex Music.py

--- YaPr 11.10.1-4 Yandex Music.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv('music_log_upd.csv')
-df.tail()
-df.tail().groupby('user_id')
-#print(df) #провека работает
-df.tail().groupby('user_id').sum() 
-current_engagement = df.tail().groupby('user_id').sum().median()
-print(current_engagement) #выходит ошибка при выводе
-
